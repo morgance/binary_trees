90eeb2fefc46dedd9d5875a04194416760e403ea
morgance <mogeokumu@gmail.com>
2023-09-01 01:56:45 -0700

    betty error

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -17,7 +17,7 @@ int binary_tree_is_parent(const binary_tree_t *node)
 }
 
 /**
- * binary_tree_nodes - counts the nodes with at least one child in a binary tree
+ * binary_tree_nodes - counts  nodes with at least 1 child in binary tree
  *
  * @tree: tree root
  * Return: number of parent nodes in a the tree or 0 if tree is NULL;
